8426fc68429478eea9a309fc80b701684cec8c97
duanedesign <duanedesign@gmail.com>
2010-07-26 15:36:39 +0000

    added TreeStore to make commands easier to find. This breaks the add/remove feature

--- clicompanion.0.0.7.py
+++ clicompanion.0.0.7.py
@@ -20,34 +20,33 @@
 # IMPORTANT: you need to move the .cheatsheet file to your $HOME
 #
 import os
-import webbrowser
-
+import gobject
 import gtk
 import pygtk
 import vte
 
 pygtk.require('2.0')
 
-states = []
-row = ''
+STATES = []
+ROW = ''
 #text=""
-cheatsheet = os.path.expanduser("~/.clicompanion")
-CONFIG_ORIG = "/usr/clicompanion.d/clicompanion.config"
+CHEATSHEET = os.path.expanduser("~/.clicompanion")
+CONFIG_ORIG = "/etc/clicompanion.d/clicompanion.config"
 
 
 class Companion:
-    
-    if os.path.isfile (cheatsheet):
-        pass
-    else:
-        os.system ("copy %s %s" % (CONFIG_ORIG, cheatsheet))    
 
-    # create the terminal
+    # create the terminal and set its size
     vte = vte.Terminal()
     vte.set_size_request(700, 350)
     # fork_command() will run a command, in this case it shows a prompt
     vte.fork_command('bash')
-
+    
+    #copy config file to user $HOME if does not exist
+    def setup(self):
+    	if not os.path.isfile (CHEATSHEET):
+            os.system ("cp %s %s" % (CONFIG_ORIG, CHEATSHEET))
+            
     # close the window and quit
     def delete_event(self, widget,  data=None):
         gtk.main_quit()
@@ -56,8 +55,8 @@ class Companion:
     # Info Dialog Box    
     # if a command needs more info EX: a package name
     def get_info(self, widget, data=None):
-        global row
-        row_int = int(row[0][0])
+        global ROW
+        row_int = int(ROW[0][0])
 
         # Create Dialog object
         dialog = gtk.MessageDialog(
@@ -98,7 +97,7 @@ class Companion:
     def responseToDialog(self, text, dialog, response):
 	    dialog.response(response)
 	
-	# Add command dialog box
+    # Add command dialog box
     def add_command(self, widget, data=None):
         # Create Dialog object
         dialog = gtk.MessageDialog(
@@ -108,7 +107,7 @@ class Companion:
             gtk.BUTTONS_OK,
             None)
 
-        # ask for input. Use column 2 for what is required
+        # primaary text
         dialog.set_markup("Add a command to your clicompanion dictionary")
 
         #create the text input field
@@ -136,9 +135,7 @@ class Companion:
         #add it and show it
         dialog.vbox.pack_end(hbox2, True, True, 0)
         dialog.vbox.pack_end(hbox1, True, True, 0)
-
         dialog.show_all()
-
         # Show the dialog
         dialog.run()
         
@@ -146,41 +143,45 @@ class Companion:
         text1 = entry1.get_text()
         text2 = entry2.get_text()
         text3 = entry3.get_text()
-        # open flat file that contains the command dictionary
-        with open(cheatsheet, "a") as cheatfile:
-            cheatfile.write(text1+" :"+text2+" : "+text3+'\n')
-            cheatfile.close()
-            self.update()
-            
+        
+        # open flat file that contains the commands and add the new command
+        with open(CHEATSHEET, "a") as cheatfile:
+            if text1 != "":
+                cheatfile.write(text1+" :"+text2+" : "+text3+'\n')
+                cheatfile.close()
+                l = str(text1+" :"+text2+" : "+text3)
+                ls = l.split(':',2)
+                STATES.append(ls[0])
+                self.liststore.append([ls[0],ls[1],ls[2]])
+          
         # The destroy method must be called otherwise the 'Close' button will
         # not work.
         dialog.destroy()
         #return text
         
-	# Remove command method 
+	# Remove command from command file and GUI 
     def remove_command(self, widget, data=None):
-        row_int = int(row[0][0]) #convert pathlist into something usable    
-        del self.liststore[row_int] #remove line from list
+        row_int = int(ROW[0][0]) #convert pathlist into something usable    
+        del self.treestore[row_int] #remove line from list
         
         # open command file and delete line so the change is persistent
-        with open(cheatsheet, "r") as cheatfile:
+        with open(CHEATSHEET, "r") as cheatfile:
             cheatlines = cheatfile.readlines()
             del cheatlines[row_int]
             cheatfile.close()
-        with open(cheatsheet, "w") as cheatfile2:           
+        with open(CHEATSHEET, "w") as cheatfile2:           
             cheatfile2.writelines(cheatlines)
             cheatfile2.close()
-
 
         
     #send the command to the terminal
     def run_command(self, widget, data=None):
-        #global row
+        global ROW
         text = ""
-        row_int = int(row[0][0]) # removes everything but number from EX: [5,]
+        row_int = int(ROW[0][0]) # removes everything but number from EX: [5,]
         
-        cmnd = states[row_int] #states is where commands are stored
-        if not self.liststore[row_int][1] == " ": # command with user input
+        cmnd = STATES[row_int] #STATES is where commands are stored
+        if not self.treestore[row_int][1] == " ": # command with user input
             text = Companion.get_info(self, text)
             #print text #debug
             Companion.vte.feed_child(cmnd+" "+text+"\n") #send command w/ input
@@ -190,14 +191,10 @@ class Companion:
             Companion.vte.show()
             Companion.vte.grab_focus()
      
-     #open the website that is the help file
-    def open_site(self, widget, data=None):
-        '''
-        siteOpen = webbrowser.open("http://okiebuntu.homelinux.com")
-        return siteOpen
-        '''
-        row_int = int(row[0][0]) # removes everything but number from EX: [5,]
-        cmnd = states[row_int] #states is where commands are store
+     #open the man page for selected command
+    def man_page(self, widget, data=None):
+        row_int = int(ROW[0][0]) # removes everything but number from EX: [5,]
+        cmnd = STATES[row_int] #STATES is where commands are store
         splitcommand=cmnd.split(" ")
         Companion.vte.feed_child("man "+splitcommand[0]+"\n") #send command
         Companion.vte.show()
@@ -205,24 +202,41 @@ class Companion:
         
     # open file containing command dictionary and put it in a variable
     def update(self):
-        with open(cheatsheet, "r") as cheatfile:
+        bugdata=[]
+        with open(CHEATSHEET, "r") as cheatfile:
             bugdata=cheatfile.read()
+            #print bugdata.__class__
             cheatfile.close()
-    
-        #global states
-        # add bug data from .clicompanion to the liststore
-        self.states = []
-        for line in sorted(bugdata.splitlines()):
-            l = line.split(':',2)
-            states.append(l[0])
-            self.liststore.append([l[0],l[1],l[2]])
-  
+
+        iter1 = self.treestore.append(None, ['package managment', ' ', ' ',' '])
+        iter2 = self.treestore.append(None, ['system managment', ' ', ' ',' '])
+        iter3 = self.treestore.append(None, ['misc. commands', ' ', ' ',' '])
         
-            
+        global STATES
+        # add bug data from .clicompanion to the liststore
+        STATES = []
+        for line in bugdata.splitlines():
+            print line
+            l = line.split(':')
+            #print l[3]
+            STATES.append(l[0])
+            if l[3] == " package":
+                print "1"
+                self.treestore.append(iter1,([l[0],l[1],l[2],l[3]]))
+            elif l[3] == " system":
+                print "2"
+                self.treestore.append(iter2,([l[0],l[1],l[2],l[3]]))
+            elif l[3] == " other":
+                print "3"
+                self.treestore.append(iter3,([l[0],l[1],l[2],l[3]]))
+
+
     def __init__(self):
+                
+        self.setup()
         # Create a new window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-        self.window.set_title("CLI Companion")              
+        self.window.set_title("CLI Companion")
         # Sets the border width of the window.
         self.window.set_border_width(10)
         #set the size of the window
@@ -235,14 +249,14 @@ class Companion:
         self.window.connect("delete_event", self.delete_event)
 
         # create a liststore with three string columns
-        self.liststore = gtk.ListStore(str, str, str)        
-        self.update()
+        self.treestore = gtk.TreeStore(str, str, str, gobject.TYPE_PYOBJECT)        
         
         #this was for the search
         #self.modelfilter = self.liststore.filter_new()
 
         # create the TreeView
-        self.treeview = gtk.TreeView()
+        self.treeview = gtk.TreeView(self.treestore)
+        self.update()
 
         # create the TreeViewColumns to display the data
         self.treeview.columns = [None]*3
@@ -251,7 +265,7 @@ class Companion:
         self.treeview.columns[2] = gtk.TreeViewColumn('Description')
         
         # The set_model() method sets the "model" property for the treeview to the value of model. model : the new tree model to use with the treeview
-        self.treeview.set_model(self.liststore)
+        self.treeview.set_model(self.treestore)
         #self.treeview.set_search_entry(entry=None)
         #self.treeview.set_enable_search(enable_search)
 
@@ -263,7 +277,7 @@ class Companion:
             # add the cells to the columns
             self.treeview.columns[n].pack_start(self.treeview.columns[n].cell,
                                                 True)
-            # set the cell attributes to the appropriate liststore column
+            # set the cell attributes to the appropriate treestore column
             self.treeview.columns[n].set_attributes(
                 self.treeview.columns[n].cell, text=n)   
             self.treeview.columns[n].set_resizable(True)          
@@ -271,12 +285,11 @@ class Companion:
         self.treeview.get_selection().set_mode(gtk.SELECTION_SINGLE)
         self.treeview.get_selection().connect('changed',lambda s: mark_selected(s)) 
         
-             
         
         def mark_selected(treeselection):
             (model,pathlist)=treeselection.get_selected_rows()
-            global row
-            row = pathlist
+            global ROW
+            ROW = pathlist
             #print pathlist #debug
 
         # make ui layout
@@ -285,7 +298,7 @@ class Companion:
         self.scrolledwindow = gtk.ScrolledWindow()
         self.scrolledwindow.set_size_request(700, 220)
         
-        def buttonBox(self, spacing, layout):
+        def button_box(self, spacing, layout):
             #button box at bottom of main window
             frame = gtk.Frame()
             bbox = gtk.HButtonBox()
@@ -314,14 +327,14 @@ class Companion:
             #Help Button
             buttonHelp = gtk.Button(stock=gtk.STOCK_HELP)
             bbox.add(buttonHelp)
-            buttonHelp.connect("clicked", self.open_site)
+            buttonHelp.connect("clicked", self.man_page)
             
             return frame
 
         
         self.vbox.pack_start(self.scrolledwindow)
         self.vbox.pack_start(self.vte, True, True, 0)
-        self.vbox.pack_start(buttonBox( self, 10, gtk.BUTTONBOX_END), True, True, 5)
+        self.vbox.pack_start(button_box( self, 10, gtk.BUTTONBOX_END), True, True, 5)
 
         self.scrolledwindow.add(self.treeview)
         self.window.add(self.vbox)
@@ -332,7 +345,7 @@ class Companion:
 def main():
     gtk.main()
 
-if __name__ == "__main__":
+if __name__ == "__main__":       
     companion = Companion()
     main()
 
