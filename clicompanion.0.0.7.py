bd92c9883806f9eedd0f498c873dff44ed7fc85c
duanedesign <duanedesign@gmail.com>
2010-07-28 12:17:33 +0000

    reverted to rev.18 TreeStore idea moved to seperate experimental branch

--- clicompanion.0.0.7.py
+++ clicompanion.0.0.7.py
@@ -3,7 +3,7 @@
 #
 # clicompanion.py - commandline tool.
 #
-# Copyright 2010 Duane Hinnen
+# Copyright 2010 Duane Hinnen, Kenny Meyer
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
@@ -20,7 +20,6 @@
 # IMPORTANT: you need to move the .cheatsheet file to your $HOME
 #
 import os
-import gobject
 import gtk
 import pygtk
 import vte
@@ -32,11 +31,7 @@ ROW = ''
 #text=""
 CHEATSHEET = os.path.expanduser("~/.clicompanion")
 CONFIG_ORIG = "/etc/clicompanion.d/clicompanion.config"
-iter1=""
-iter2=""
-iter3=""
-iter4=""
-text4=""
+
 
 class Companion:
 
@@ -52,7 +47,7 @@ class Companion:
             os.system ("cp %s %s" % (CONFIG_ORIG, CHEATSHEET))
             
     # close the window and quit
-    def delete_event(self, widget,  data=None):
+    def quit_event(self, widget,  data=None):
         gtk.main_quit()
         return False
         
@@ -76,7 +71,7 @@ class Companion:
         #create the text input field
         entry = gtk.Entry()
         #allow the user to press enter to do ok
-        entry.connect("activate", self.responseToDialog, dialog, gtk.RESPONSE_OK)
+        entry.connect("activate", self.response_to_dialog, dialog, gtk.RESPONSE_OK)
 
         #create a horizontal box to pack the entry and a label
         hbox = gtk.HBox()
@@ -98,12 +93,11 @@ class Companion:
         dialog.destroy()
         return text
 
-    def responseToDialog(self, text, dialog, response):
+    def response_to_dialog(self, text, dialog, response):
 	    dialog.response(response)
 	
     # Add command dialog box
     def add_command(self, widget, data=None):
-        global iter4
         # Create Dialog object
         dialog = gtk.MessageDialog(
             None,
@@ -120,7 +114,7 @@ class Companion:
         entry2 = gtk.Entry()
         entry3 = gtk.Entry()
         #allow the user to press enter to do ok
-        entry1.connect("activate", self.responseToDialog, dialog, gtk.RESPONSE_OK)
+        entry1.connect("activate", self.response_to_dialog, dialog, gtk.RESPONSE_OK)
 
         #create the three labels
         hbox1 = gtk.HBox()
@@ -134,18 +128,6 @@ class Companion:
         hbox2.pack_start(gtk.Label("Description"), False, 5, 5)
         hbox2.pack_start(entry3, True, 5, 5)
     
-    
-        combobox = gtk.combo_box_new_text()
-        hbox2.add(combobox)
-        combobox.append_text('select a category')
-        combobox.append_text('package managment')
-        combobox.append_text('system managment')
-        combobox.append_text('misc. commands')
-        combobox.connect('changed', self.changed_cb)
-        combobox.set_active(0)
-
-        
-        
         #some secondary text
         dialog.format_secondary_markup("Please provide a command, description, and what type of user variable if any is required.")
         
@@ -160,45 +142,27 @@ class Companion:
         text1 = entry1.get_text()
         text2 = entry2.get_text()
         text3 = entry3.get_text()
-
         
         # open flat file that contains the commands and add the new command
         with open(CHEATSHEET, "a") as cheatfile:
             if text1 != "":
-                cheatfile.write(text1+" :"+text2+" : "+text3+" : "+text4+"\n")
+                cheatfile.write(text1+" :"+text2+" : "+text3+'\n')
                 cheatfile.close()
-                l = str(text1+" :"+text2+" : "+text3+" : "+text4+"\n")
-                ls = l.split(':',3)
+                l = str(text1+" :"+text2+" : "+text3)
+                ls = l.split(':',2)
                 STATES.append(ls[0])
-                self.treestore.append(iter4,([ls[0],ls[1],ls[2],ls[3]]))
+                self.liststore.append([ls[0],ls[1],ls[2]])
+            #self.update()
           
         # The destroy method must be called otherwise the 'Close' button will
         # not work.
         dialog.destroy()
         #return text
         
-        
-    def changed_cb(self, combobox):
-        global iter1, iter2, iter3, iter4, text4
-        model = combobox.get_model()
-        index = combobox.get_active()
-        if index:
-            if model[index][0] == "package managment":
-                iter4 = iter1
-                text4 = "package"
-            if model[index][0] == "system managment":
-                iter4 = iter2
-                text4 = "system"
-            if model[index][0] == "misc. commands":
-                iter4 = iter3
-                text4 = "other"
-        return
-
-        
 	# Remove command from command file and GUI 
     def remove_command(self, widget, data=None):
         row_int = int(ROW[0][0]) #convert pathlist into something usable    
-        del self.treestore[row_int] #remove line from list
+        del self.liststore[row_int] #remove line from list
         
         # open command file and delete line so the change is persistent
         with open(CHEATSHEET, "r") as cheatfile:
@@ -209,6 +173,9 @@ class Companion:
             cheatfile2.writelines(cheatlines)
             cheatfile2.close()
 
+
+
+
         
     #send the command to the terminal
     def run_command(self, widget, data=None):
@@ -217,7 +184,7 @@ class Companion:
         row_int = int(ROW[0][0]) # removes everything but number from EX: [5,]
         
         cmnd = STATES[row_int] #STATES is where commands are stored
-        if not self.treestore[row_int][1] == " ": # command with user input
+        if not self.liststore[row_int][1] == " ": # command with user input
             text = Companion.get_info(self, text)
             #print text #debug
             Companion.vte.feed_child(cmnd+" "+text+"\n") #send command w/ input
@@ -238,38 +205,21 @@ class Companion:
         
     # open file containing command dictionary and put it in a variable
     def update(self):
-        global iter1, iter2, iter3
-        bugdata=[]
         with open(CHEATSHEET, "r") as cheatfile:
             bugdata=cheatfile.read()
-            #print bugdata.__class__
             cheatfile.close()
-
-        iter1 = self.treestore.append(None, ['package managment', ' ', ' ',' '])
-        iter2 = self.treestore.append(None, ['system managment', ' ', ' ',' '])
-        iter3 = self.treestore.append(None, ['misc. commands', ' ', ' ',' '])
-        
+    
         global STATES
         # add bug data from .clicompanion to the liststore
         STATES = []
-        for line in bugdata.splitlines():
-            #print line
-            l = line.split(':')
-            #print l[3]
+        for line in sorted(bugdata.splitlines()):
+            l = line.split(':',2)
             STATES.append(l[0])
-            if l[3] == " package":
-                #print "1"
-                self.treestore.append(iter1,([l[0],l[1],l[2],l[3]]))
-            elif l[3] == " system":
-                #print "2"
-                self.treestore.append(iter2,([l[0],l[1],l[2],l[3]]))
-            elif l[3] == " other":
-                #print "3"
-                self.treestore.append(iter3,([l[0],l[1],l[2],l[3]]))
-
-
+            self.liststore.append([l[0],l[1],l[2]])
+            
+            
     def __init__(self):
-                
+        
         self.setup()
         # Create a new window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
@@ -283,17 +233,15 @@ class Companion:
         #Allow user to resize window
         self.window.set_resizable(True)
 
-        self.window.connect("delete_event", self.delete_event)
+        self.window.connect("delete_event", self.quit_event)
 
         # create a liststore with three string columns
-        self.treestore = gtk.TreeStore(str, str, str, gobject.TYPE_PYOBJECT)        
+        self.liststore = gtk.ListStore(str, str, str)        
+        self.update()
         
-        #this was for the search
-        #self.modelfilter = self.liststore.filter_new()
 
         # create the TreeView
-        self.treeview = gtk.TreeView(self.treestore)
-        self.update()
+        self.treeview = gtk.TreeView()
 
         # create the TreeViewColumns to display the data
         self.treeview.columns = [None]*3
@@ -302,7 +250,7 @@ class Companion:
         self.treeview.columns[2] = gtk.TreeViewColumn('Description')
         
         # The set_model() method sets the "model" property for the treeview to the value of model. model : the new tree model to use with the treeview
-        self.treeview.set_model(self.treestore)
+        self.treeview.set_model(self.liststore)
         #self.treeview.set_search_entry(entry=None)
         #self.treeview.set_enable_search(enable_search)
 
@@ -314,7 +262,7 @@ class Companion:
             # add the cells to the columns
             self.treeview.columns[n].pack_start(self.treeview.columns[n].cell,
                                                 True)
-            # set the cell attributes to the appropriate treestore column
+            # set the cell attributes to the appropriate liststore column
             self.treeview.columns[n].set_attributes(
                 self.treeview.columns[n].cell, text=n)   
             self.treeview.columns[n].set_resizable(True)          
@@ -360,7 +308,7 @@ class Companion:
             # Cancel button
             buttonCancel = gtk.Button(stock=gtk.STOCK_QUIT)
             bbox.add(buttonCancel)
-            buttonCancel.connect("clicked", self.delete_event)
+            buttonCancel.connect("clicked", self.quit_event)
             #Help Button
             buttonHelp = gtk.Button(stock=gtk.STOCK_HELP)
             bbox.add(buttonHelp)
@@ -368,11 +316,9 @@ class Companion:
             
             return frame
 
-
-
-
         
         self.vbox.pack_start(self.scrolledwindow)
+
         self.vbox.pack_start(self.vte, True, True, 0)
         self.vbox.pack_start(button_box( self, 10, gtk.BUTTONBOX_END), True, True, 5)
 
