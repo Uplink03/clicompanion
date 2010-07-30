7f935749d1b3293751e7b8bd17fa680f6da194f5
Kenny Meyer <knny.myer@gmail.com>
2010-07-30 14:27:33 -0400

    [FIX] Removed unnecessary IMPORTANT message in script header

--- clicompanion.0.0.7.py
+++ clicompanion.0.0.7.py
@@ -16,9 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# IMPORTANT: you need to move the .cheatsheet file to your $HOME
-#
 import os
 import gtk
 import pygtk
