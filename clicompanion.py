eb2806da299f4d2bc2c379ec00f238afb56a58c7
duanedesign <duanedesign@gmail.com>
2010-08-27 16:39:59 +0000

    mv po => locale and added test es translation

--- clicompanion.py
+++ clicompanion.py
@@ -46,14 +46,14 @@ def get_language():
     return
 
 locale_dir = os.path.join(BASEDIR, "locale")
-gettext.install('core', locale_dir, unicode=True)
+gettext.install('clicompanion', locale_dir, unicode=True)
 idioma = get_language()
 if idioma is not None:
-    mo = os.path.join(locale_dir, '%s/LC_MESSAGES/core.mo' % idioma)
+    mo = os.path.join(locale_dir, '%s/LC_MESSAGES/clicompanion.mo' % idioma)
     if not os.access(mo, os.F_OK):
         raise IOError("The l10n directory (for language %r) exists but "
-                      "not the core.mo file" % idioma)
-    trans = gettext.translation('core', locale_dir, languages=[idioma])
+                      "not the clicompanion.mo file" % idioma)
+    trans = gettext.translation('clicompanion', locale_dir, languages=[idioma])
     trans.install(unicode=True)
 
 # End with i18n
