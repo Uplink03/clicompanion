6f1bcb56cb1b4fd9ade4876fa6a902f5f9b940c0
duanedesign <duanedesign@gmail.com>
2010-10-06 06:54:47 +0000

    created directory for menus_buttons.py and utils.py

---
+++ clicompanion/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# clicompanion.py - commandline tool.
+#
+# Copyright 2010 Duane Hinnen, Kenny Meyer
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+'''
+blah, blah, blah
+'''
---
+++ clicompanion/utils.py
@@ -0,0 +1,66 @@
+# -*- mode: python; coding: utf-8; -*-
+#
+# clicompanion.py - commandline tool.
+#
+# Copyright 2010 Duane Hinnen, Kenny Meyer, Marcos Vanetta
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+"""
+A collection of useful functions.
+"""
+
+import getpass
+import hashlib
+import os
+
+CHEATSHEET = os.path.expanduser("~/.clicompanion")
+CONFIG_ORIG = "/etc/clicompanion.d/clicompanion.config"
+
+def get_user_shell():
+    """Get the user's shell defined in /etc/passwd ."""
+    data = None
+    try:
+        # Read out the data in /etc/passwd
+        with open('/etc/passwd') as f:
+            data = f.readlines()
+    except e:
+        print "Something unexpected happened!"
+        raise e
+
+    for i in data:
+        tmp = i.split(":")
+        # Check for the entry of the currently logged in user
+        if tmp[0] == getpass.getuser(): 
+            # Columns are separated by colons, so split each column.
+            # Sample /etc/passwd entry for a user:
+            # 
+            #  jorge:x:1001:1002:,,,:/home/jorge:/bin/bash
+            #
+            # The last column is relevant for us.
+            # Don't forget to strip the newline at the end of the string!
+            return i.split(":")[-1:][0].strip('\n')
+            
+            
+def sumfile():
+    '''Returns an md5 hash for an object with read() method.'''
+    m = hashlib.md5()
+    with open(CHEATSHEET, 'rb') as cheatsheet:
+        d = cheatsheet.read()
+        m.update(d)
+        pp = m.hexdigest()
+        print pp
+    return m.hexdigest()
+
